4cbf2afd36af7d8da0283b618f18ba28f7de5dab
owenborders <owen.borders2@gmail.com>
2024-09-16 16:16:10 -0400

    added class to evaluate model performance

---
+++ evaluation/evaluate_model.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+import tensorflow as tf
+import nibabel as nib
+from PIL import Image
+import random
+from tensorflow.keras.models import load_model
+
+class EvaluateModel():
+
+    def __init__(self):
+        self.model_path = ''
+
+    def run_script(self):
+        self.load_model()
+
+    def load_model(self):
+        pass
+
+
+if __name__ == '__main__':
+    EvaluateModel().run_script()
