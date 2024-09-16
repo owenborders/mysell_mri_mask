af01ec79e1e4c5f69bb519d314a68682f1a9c3a9
owenborders <owen.borders2@gmail.com>
2024-09-16 16:13:49 -0400

    added evaluation folder

--- augmentation/known_artifacts.py
+++ augmentation/known_artifacts.py
@@ -188,24 +188,6 @@ class KnownArtifacts():
         return scan_img
 
 
-      def elastic_transform_opencv(self,image, alpha, sigma):
-        random_state = np.random.RandomState(None)
-
-        shape = image.shape
-        dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), \
-        sigma, mode='constant', cval=0) * alpha
-        dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), \
-        sigma, mode='constant', cval=0) * alpha
-
-        x, y = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]))
-        map_x = (x + dx).astype(np.float32)
-        map_y = (y + dy).astype(np.float32)
-
-        distorted_image = cv2.remap(image, map_x, map_y, interpolation=cv2.INTER_LINEAR)
-
-        return distorted_image
-
-
       def overlay_random_noise(self, scan_img, mask_img, absolute_max,absolute_min, brain_only) -> None:
         """
         Adds noise over the
