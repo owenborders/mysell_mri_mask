456a73632c4091e7b47edec373dd690a1e9ff33f
owenborders <owen.borders2@gmail.com>
2024-09-17 11:14:53 -0400

    removed elastic transform function

--- augmentation/normal_variations.py
+++ augmentation/normal_variations.py
@@ -10,25 +10,21 @@ class NormalVariations():
 
     def __init__(self):
         self.utils = Utils()
-
-    def elastic_transform(self,image, alpha, sigma):
-        random_state = np.random.RandomState(None)
-
-        shape = image.shape
-        dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), \
-        sigma, mode='constant', cval=0) * alpha
-        dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), \
-        sigma, mode='constant', cval=0) * alpha
-
-        x, y = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]))
-        map_x = (x + dx).astype(np.float32)
-        map_y = (y + dy).astype(np.float32)
-
-        distorted_image = cv2.remap(image, map_x, map_y, interpolation=cv2.INTER_LINEAR)
-
-        return distorted_image
     
     def warp_image(self,img, initial_points, transformed_points):
+        """
+        Warps image by transforming the 
+        initial points into the transformed points.
+
+        Parameters
+        ---------------
+        img : np.array
+            Input image to be transforms
+        initial_points : list
+            Initial coordinates that will be moved
+        transformed_pints : list
+            Coordinates that the initial points arer being transformed into
+        """
         rows, cols = img.shape[:2]
 
         pts1 = np.float32(initial_points)
