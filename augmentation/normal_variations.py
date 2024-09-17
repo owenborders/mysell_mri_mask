09e8b61445b07672063585f0a50982dc218648d8
owenborders <owen.borders2@gmail.com>
2024-09-17 18:16:24 -0400

    added function to create random textures from skull in background

--- augmentation/normal_variations.py
+++ augmentation/normal_variations.py
@@ -6,6 +6,7 @@ from scipy.ndimage import gaussian_filter
 
 import random
 from scipy.ndimage import rotate,zoom
+import copy
 
 class NormalVariations():
 
@@ -39,22 +40,16 @@ class NormalVariations():
     
 
     def randomize_resolution(image):
-
         height, width = image.shape[:2]
-
         scale_factor = random.uniform(0.3, 0.3)
-
         new_width = int(width * scale_factor)
         new_height = int(height * scale_factor)
-
         downscaled_image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
-
         upscaled_image = cv2.resize(downscaled_image, (width, height), interpolation=cv2.INTER_LINEAR)
 
         return upscaled_image
 
     def create_warp_ranges(self, num_points, variation_range):
-
         orig_points = [[50, 50], [200, 50], [50, 200], [200, 200]]
         transformed_points = []
         for point_ind in range(0, num_points):
@@ -100,14 +95,29 @@ class NormalVariations():
         - mean_intensity) * contrast_factor + mean_intensity
         np.clip(self.slice_image, 0, 1, out=self.slice_image)
 
+    def add_skull_textures(self):
+        skull_only = copy.deepcopy(self.slice_image)
+        skull_only[self.mask_image > 0] = 0
+
+        for x in range(0,2):
+            pts1, pts2 = self.create_warp_ranges(self, skull_only, 4, 100)
+            skull_only = self.standard_transformations.rotate_image(skull_only, random.randint(0,360))
+
+            skull_only = self.standard_transformations.warp_image(skull_only, pts1, pts2)
+
+        
+
+        self.slice_image[self.slice_image < 0.1] = skull_only[self.slice_image < 0.1]
+
+
 
     def convert_to_mp2rage(self):     
         smaller_mask = self.utils.resize(self.mask_image, 0.9)
         self.slice_image[(self.slice_image > 0.1) & (self.mask_image < 0.05)] +=0.3
         self.change_brain_contrast(2)
         #self.slice_image[(self.slice_image < 0.1) & (self.mask_image > 0.05) & (smaller_mask < 0.05)] += 0.45
-        self.slice_image[(self.slice_image < 0.1) & (self.mask_image > 0.05)] += random.uniform(0.1,0.5)
-
+        self.slice_image[(self.slice_image < 0.1) & \
+        (self.mask_image > 0.05)] += random.uniform(0.1,0.5)
         #self.slice_image[(self.slice_image < 0.1) & (self.mask_image > 0.05)] *= random.uniform(1.5,3.5)
         self.change_skull_contrast(2)
         for row in range(0,len(self.slice_image)):
