ddaca2d576f18148b4e6aa244fa5de2960ca0da7
owenborders <owen.borders2@gmail.com>
2024-10-03 17:31:55 -0400

    graphed performance per slice

--- evaluation/evaluate_model.py
+++ evaluation/evaluate_model.py
@@ -10,7 +10,7 @@ from matplotlib import pyplot as plt
 import cv2
 import sys
 import os, sys
-
+import pandas as pd
 import copy
 sys.path.insert(0, os.path.abspath(".."))
 
@@ -29,25 +29,94 @@ class EvaluateModel():
     def __init__(self, model_path, test_set_directory):
         self.model_path = model_path
         self.performance_per_slice = {'axial':{},'sagittal':{},'coronal':{}}
+        self.performance_per_slice_percentage = {'axial':{},'sagittal':{},'coronal':{}}
         self.test_set_directory = test_set_directory
         self.all_dice_losses = []
 
         self.utils = Utils()
+
+        self.mask_lengths = {'axial':{'start':0,'end':0},\
+        'sagittal':{'start':0,'end':0},'coronal':{'start':0,'end':0}}
+
+
+    def dice_coefficient_nifti(self,true_mask_path, pred_mask_path):
+        # Load the true mask and predicted mask NIfTI files
+        true_mask_nii = nib.load(true_mask_path)
+        pred_mask_nii = nib.load(pred_mask_path)
+        
+        # Get the data from the NIfTI files
+        true_mask = true_mask_nii.get_fdata().astype(np.bool_)
+        pred_mask = pred_mask_nii.get_fdata().astype(np.bool_)
+        
+        # Calculate intersection and sum of both masks
+        intersection = np.logical_and(true_mask, pred_mask).sum()
+        total_sum = true_mask.sum() + pred_mask.sum()
+        
+        # Calculate Dice coefficient
+        if total_sum == 0:
+            return 1.0  # If both masks are empty, return a perfect Dice score
+        else:
+            dice = 2 * intersection / total_sum
+            return dice
+
         
 
     def run_script(self):
-        self.load_model()
+        dice_list =[]
+        oc_subs = []
+        oc_df = pd.read_excel('/data/pnlx/projects/mysell_masking_cnn/oc_subjects.xlsx',keep_default_na = False)
+        for row in oc_df.itertuples():
+            print(row.checked)
+            if row.checked == True:
+                oc_subs.append(row.Subject)
+        print(oc_subs)
+        print(len(oc_subs))
+        oc_par_dir = '/data/pnlx/home/kc1591/OC/derivatives/pnlpipe/sub-startOC967t0/ses-01/anat/'
+        for sub in oc_subs:
+            if 'startOC971t1' in sub:
+                continue
+            self.input_path = f'/data/pnlx/home/kc1591/OC/derivatives/pnlpipe/{sub}/ses-01/anat/{sub}_ses-01_desc-T1wXcMabsQc_mask.nii.gz'
+            self.output_path = f'/data/pnlx/projects/mysell_masking_cnn/oc_predictions/{sub}_mask_test.nii.gz'
+            if not os.path.exists(self.input_path) or not os.path.exists(self.output_path):
+                print(self.input_path)
+                print(self.output_path)
+                continue
+            # EXCLUDE sub-startOC971t1_ses-01_desc-T1wXcMabsQc_mask.nii.gz
+            dice = self.dice_coefficient_nifti(self.input_path,self.output_path)
+            dice_list.append(dice)
+            print('----')
+            print(self.input_path)
+            print(dice_list)
+            print(np.mean(dice_list))
+        #self.load_model()
+
         #self.loop_directory()
-        parent_dir = '/data/pnlx/projects/mysell_masking_cnn/NFBS_Dataset/'
+        """parent_dir = '/data/pnlx/projects/mysell_masking_cnn/NFBS_Dataset/'
         for sub in os.listdir(parent_dir):
+            self.performance_per_slice = {'axial':{},'sagittal':{},'coronal':{}}
+            self.mask_lengths = {'axial':{'start':0,'end':0},\
+            'sagittal':{'start':0,'end':0},'coronal':{'start':0,'end':0}}
             print(sub)
             sub_dir = f'{parent_dir}{sub}/'
             #sub_dir = '/data/pnlx/projects/mysell_masking_cnn/NFBS_Dataset/A00064081/'
 
             self.load_niftii_file(f'{sub_dir}/sub-{sub}_ses-NFB3_T1w.nii.gz',\
-                                f'{sub_dir}/sub-{sub}_ses-NFB3_T1w_brainmask.nii.gz')
+                                f'{sub_dir}/sub-{sub}_ses-NFB3_T1w_brainmask_refined.nii.gz')
             
-            print(self.performance_per_slice)
+            #print(self.performance_per_slice)
+            for view,slices in self.performance_per_slice.items():
+                count = 0
+                length = self.mask_lengths[view]['end'] - self.mask_lengths[view]['start']
+                for slice_count, dice in slices.items():
+                    if self.mask_lengths[view]['start'] <= slice_count <=self.mask_lengths[view]['end']:
+                        percentage = round((count/length) * 100, 0)
+                        self.performance_per_slice_percentage[view].setdefault(percentage,0)
+                        self.performance_per_slice_percentage[view][percentage] += dice
+                        count +=1
+
+            print('------------------------------------')
+            print(self.performance_per_slice_percentage)"""
+
 
     def dice_loss(self, y_true, y_pred):
         smooth = 1e-6  
@@ -119,6 +188,7 @@ class EvaluateModel():
             mask_array = np.load(self.test_set_directory + f'{sub}/mask_array.npy')
             self.test_accuracy(mask_array, mri_array)
 
+
     def load_niftii_file(self, mri_path, mask_path):
         nii_data_array = nib.load(mri_path)
         nii_data_array = nii_data_array.get_fdata()
@@ -154,6 +224,15 @@ class EvaluateModel():
                     self.performance_per_slice[view_list[view]].setdefault(slice_count,0)
                     self.performance_per_slice[view_list[view]][slice_count] += output
                     
+                    if self.mask_lengths[view_list[view]]['start'] == 0:
+                        if np.sum(curr_mask) >0:
+                            self.mask_lengths[view_list[view]]['start'] = slice_count
+
+                    if self.mask_lengths[view_list[view]]['start'] != 0 \
+                    and self.mask_lengths[view_list[view]]['end'] == 0:
+                        if np.sum(curr_mask) == 0:
+                            self.mask_lengths[view_list[view]]['end'] = slice_count - 1
+
 
                 elif view == 1 and slice_count < nii_data_array.shape[view]:
                     print(view)
@@ -170,6 +249,17 @@ class EvaluateModel():
 
                     self.performance_per_slice[view_list[view]].setdefault(slice_count,0)
                     self.performance_per_slice[view_list[view]][slice_count] += output
+
+                    if self.mask_lengths[view_list[view]]['start'] == 0:
+                        if np.sum(curr_mask) >0:
+                            self.mask_lengths[view_list[view]]['start'] = slice_count
+
+                    if self.mask_lengths[view_list[view]]['start'] != 0 \
+                    and self.mask_lengths[view_list[view]]['end'] == 0:
+                        if np.sum(curr_mask) == 0:
+                            self.mask_lengths[view_list[view]]['end'] = slice_count - 1
+
+
 
                     
                 elif view == 2 and slice_count < nii_data_array.shape[view]:
@@ -190,6 +280,16 @@ class EvaluateModel():
                     self.performance_per_slice[view_list[view]].setdefault(slice_count,0)
                     self.performance_per_slice[view_list[view]][slice_count] += output
 
+                    if self.mask_lengths[view_list[view]]['start'] == 0:
+                        if np.sum(curr_mask) >0:
+                            self.mask_lengths[view_list[view]]['start'] = slice_count
+
+                    if self.mask_lengths[view_list[view]]['start'] != 0 \
+                    and self.mask_lengths[view_list[view]]['end'] == 0:
+                        if np.sum(curr_mask) == 0:
+                            self.mask_lengths[view_list[view]]['end'] = slice_count - 1
+
+        print(self.mask_lengths)
 
 
     def apply_threshold(self, preds, threshold = 0.5):
@@ -248,5 +348,6 @@ class EvaluateModel():
 
 if __name__ == '__main__':
     model_path = '/data/pnlx/projects/mysell_masking_cnn/final_results/models/attention_unet_stage_one_best_only.h5'
+    model_path = '/data/pnlx/projects/mysell_masking_cnn/final_results/models/attention_unet_stage_one_refined_best_only.h5'
     test_set_directory = '/data/pnlx/projects/mysell_masking_cnn/training_set_stage_2/'
     EvaluateModel(model_path,test_set_directory).run_script()
